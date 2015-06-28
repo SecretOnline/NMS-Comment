6fdd00d991cea8c0ed223946623c9ddae14d8774
secret_online <SecretOnline@users.noreply.github.com>
2015-06-29 10:28:02 +1200

    First version
    Doesn't work on comment reply. Don't know why.

---
+++ background.js
@@ -0,0 +1,30 @@
+var data_words = [
+  'comment',
+  'procedure',
+  'release date',
+  'tweet',
+  'post',
+  'article'
+];
+
+function addGenerateButton(){
+  var commentAreaArray = document.querySelectorAll('.usertext-edit');
+  for (var i = 0; i < commentAreaArray.length; i++){
+    var button = commentAreaArray[i].querySelector('.procedural');
+    if (!button){
+      var buttonContainer = commentAreaArray[i].querySelector('.usertext-buttons');
+      var commentArea = commentAreaArray[i].querySelector('textarea');
+      var newButton = document.createElement('button');
+      newButton.type = 'button';
+      newButton.innerHTML = 'generate comment';
+      newButton.classList.add('procedural');
+      newButton.addEventListener('click', generateComment);
+      buttonContainer.appendChild(newButton);
+    }
+  }
+}
+function generateComment(){
+  var textArea = this.parentNode.parentNode.parentNode.querySelector('textarea');
+  textArea.value = 'Every ' + data_words[Math.floor(Math.random() * data_words.length)] + ' procedural';
+}
+window.addEventListener('DOMSubtreeModified', addGenerateButton);
